712b606caafd106194b17207b8978b7e346a6bf6
piyushdevops410 <piyushdevops410@gmail.com>
2026-09-15 16:35:09 +0530

    jenkins lab day13 CICD

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get("/health", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Application running on port ${PORT}`);
+    console.log(`Application running on port ${PORT} CICD`);
 });
